6e7b95b6470873271e2153f6a662b696e0289fe0
qngvin <quangvinh11602@gmail.com>
2024-05-12 22:59:23 +0700

    connect db

--- server/app.js
+++ server/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolver/resolver");
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://qngvin:Vinhfpt11602@tutorialgraphql.i98j0wv.mongodb.net/",
+      {}
+    );
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
 
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
@@ -10,12 +23,18 @@ async function startApolloServer() {
   const app = express();
   server.applyMiddleware({ app });
 
-  // Additional Express app setup if needed
-
   await new Promise((resolve) => app.listen({ port: 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
-startApolloServer().catch((err) =>
-  console.error("Error starting server:", err)
-);
+// Start Apollo server and connect to MongoDB
+async function startServer() {
+  try {
+    await connectDB();
+    await startApolloServer();
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,27 +1,35 @@
 const { books, authors } = require("../data/static");
+const Author = require("../models/Author");
+const Book = require("../models/Book");
 const resolvers = {
-//Query
+  //Query
   Query: {
     books: () => books,
     book: (parent, agrs) => books.find((book) => book.id == agrs.id),
     authors: () => authors,
     author: (parent, agrs) => authors.find((author) => author.id == agrs.id),
   },
-  Book:{
-    author: (parent,agrs)=>{
-        return authors.find((author) => author.id == parent.authorsId)
-    }
+  Book: {
+    author: (parent, agrs) => {
+      return authors.find((author) => author.id == parent.authorsId);
+    },
   },
-  Author:{
-    books:(parent,agrs) =>{
-       return books.filter((book)=> book.authorsId == parent.id)
-    }
+  Author: {
+    books: (parent, agrs) => {
+      return books.filter((book) => book.authorsId == parent.id);
+    },
   },
 
-//Mutation
+  //Mutation
   Mutation: {
-    createAuthor:(parent,agrs) => agrs,
-    createBook:(parent,agrs) => agrs
-  }
+    createAuthor: async (parent, agrs) => {
+      const newAuthor = new Author(agrs);
+      return await newAuthor.save();
+    },
+    createBook: async (parent, agrs) => {
+      const newBook = new Book(agrs);
+      return await newBook.save();
+    },
+  },
 };
 module.exports = resolvers;
